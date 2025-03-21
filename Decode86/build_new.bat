1cd62354fcda49075a848a9aae640e42779ea9d6
CKA <163610791+b01dc0de@users.noreply.github.com>
2025-03-20 22:52:20 -0500

    Remove Decode86

--- Decode86/build_new.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-IF NOT EXIST build mkdir build
-pushd build
-
-call cl -nologo -Zi -FC ..\clean_decode86.cpp -Feclean_decode86_debug.exe
-call cl -O2 -nologo -Zi -FC ..\clean_decode86.cpp -Feclean_decode86_release.exe
-
-popd
