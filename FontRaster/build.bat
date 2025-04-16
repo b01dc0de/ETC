a66e0fb4a06d264e3ec59b9ea2f362bb6aa4a5ea
CKA <163610791+b01dc0de@users.noreply.github.com>
2025-04-15 21:05:06 -0500

    Correctly define DEBUG_BUILD

--- FontRaster/build.bat
+++ FontRaster/build.bat
@@ -2,7 +2,7 @@
 IF NOT EXIST build mkdir build
 pushd build
 
-call cl -nologo -Zi -DUNITY_BUILD -FC ..\src\font_raster.cpp -FeFontRaster_debug.exe
+call cl -nologo -Zi -DUNITY_BUILD -DDEBUG_BUILD -FC ..\src\font_raster.cpp -FeFontRaster_debug.exe
 call cl -O2 -nologo -Zi -DUNITY_BUILD -FC ..\src\font_raster.cpp -FeFontRaster_release.exe
 
 popd
