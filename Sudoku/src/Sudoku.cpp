e49c7e592f6a6f5bb10de99b64de844fe5269e50
CKA <163610791+b01dc0de@users.noreply.github.com>
2025-04-22 16:41:49 -0500

    Add common width typedefs, add SudokuGrid+::Print

--- Sudoku/src/Sudoku.cpp
+++ Sudoku/src/Sudoku.cpp
@@ -1,8 +1,61 @@
+#include <stdint.h>
 #include <stdio.h>
+
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+
+using s8 = int8_t;
+using s16 = int16_t;
+using s32 = int32_t;
+using s64 = int64_t;
+
+using f32 = float;
+using f64 = double;
+
+struct SudokuGrid
+{
+    static constexpr u32 Stride = 9;
+    static constexpr u32 NumRows = Stride;
+    static constexpr u32 Size = Stride * NumRows;
+
+    u32 Grid[Size];
+
+    void Init()
+    {
+        for (u32 Idx = 0; Idx < Size; Idx++)
+        {
+            Grid[Idx] = 0;
+        }
+    }
+
+    void Print()
+    {
+        for (u32 RowIdx = 0; RowIdx < NumRows; RowIdx++)
+        {
+            for (u32 ColIdx = 0; ColIdx < Stride; ColIdx++)
+            {
+                printf("%u", Grid[RowIdx * Stride + ColIdx]);
+                if (ColIdx == 2 || ColIdx == 5)
+                {
+                    printf(" | ");
+                }
+            }
+            if (RowIdx == 2 || RowIdx == 5)
+            {
+                printf("\n---------------");
+            }
+            printf("\n");
+        }
+    }
+};
 
 int main()
 {
-    printf("Sudoku -- INIT\n");
+    SudokuGrid TheGrid;
+    TheGrid.Init();
+    TheGrid.Print();
 
     return 0;
 }
