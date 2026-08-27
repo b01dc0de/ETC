e3b5dbeb2b54a9de8f4dc02c3818c0034cb404ea
CKA <163610791+b01dc0de@users.noreply.github.com>
2026-08-27 14:03:27 -0500

    [etc][jsonparse] add ReadFileContents, test/example.json

--- JSONParse/src/Main.cpp
+++ JSONParse/src/Main.cpp
@@ -1,8 +1,56 @@
+#include <stdint.h>
 #include <stdio.h>
+#include <string.h>
+
+using s8 = int8_t;
+using s16 = int16_t;
+using s32 = int32_t;
+using s64 = int64_t;
+
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+
+using f32 = float;
+using f64 = double;
+
+struct FileContentsT
+{
+    u64 Size;
+    u8* Contents;
+};
+
+FileContentsT ReadFileContents(const char* FileName, bool bAppendZero = false)
+{
+    FileContentsT Result = {};
+
+    FILE* FileHandle = nullptr;
+    fopen_s(&FileHandle, FileName, "rb");
+    if (FileHandle)
+    {
+        fseek(FileHandle, 0, SEEK_END);
+        Result.Size = (u64)ftell(FileHandle);
+        fseek(FileHandle, 0, SEEK_SET);
+
+        if (Result.Size)
+        {
+            Result.Contents = new u8[Result.Size + (bAppendZero ? 1 : 0)];
+            fread_s(Result.Contents, Result.Size, Result.Size, 1, FileHandle);
+            if (bAppendZero) { Result.Contents[Result.Size - 1] = '\0'; }
+        }
+
+        fclose(FileHandle);
+    }
+
+    return Result;
+}
 
 int main()
 {
-    printf("INIT - ETC/JSONParse\n");
+    FileContentsT JSONText = ReadFileContents("test/example.json", true);
+
+    __debugbreak();
 
     return 0;
 }
